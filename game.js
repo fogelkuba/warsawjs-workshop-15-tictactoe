124c96b0bf9a7fafef9b637bf3745d33a91f3dd3
fogelkuba <fogelkuba@gmail.com>
2018-01-07 13:06:09 +0100

    step 7

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     let playerClasses = {
-        playerA: 'red',
-        playerB: 'blue'
+        playerA: 'x',
+        playerB: 'o'
     };
 
     let currentPlayer;
@@ -24,13 +24,79 @@ document.addEventListener('DOMContentLoaded', function() {
         emptyFields--;
         console.log(emptyFields);
 
-        if (!emptyFields) {
-            console.log('Koniec Gry');
-            alert('Koniec Gry!');
-        }
-
         currentPlayer = currentPlayer === 'playerA' ? 'playerB' : 'playerA';
         this.removeEventListener('click', fieldClickHandler);
+
+        // if (!emptyFields) {
+        //     console.log('Koniec Gry');
+        //     alert('Koniec Gry!');
+        // }
+
+        checkWinner();
+    };
+
+    function checkWinner(){
+        const fields = document.querySelectorAll('.board > div');
+
+        /*
+            +---+---+---+
+            | 0 | 1 | 2 |
+            +---+---+---+
+            | 3 | 4 | 5 |
+            +---+---+---+
+            | 6 | 7 | 8 |
+            +---+---+---+
+        */
+
+        //horizontal winning configurations
+        var row1 = fields[0].className + fields[1].className + fields[2].className;
+        var row2 = fields[3].className + fields[4].className + fields[5].className;
+        var row3 = fields[6].className + fields[7].className + fields[8].className;
+
+        //vertical winning configurations
+        var column1 = fields[0].className + fields[3].className + fields[6].className;
+        var column2 = fields[1].className + fields[4].className + fields[7].className;
+        var column3 = fields[2].className + fields[5].className + fields[8].className;
+
+        //diagonal winning configurations
+        var diagonal1 = fields[0].className + fields[4].className + fields[8].className;
+        var diagonal2 = fields[2].className + fields[4].className + fields[6].className;
+
+        var boardCheck = [
+            row1,
+            row2,
+            row3,
+            column1,
+            column2,
+            column3,
+            diagonal1,
+            diagonal2
+        ];
+
+        if (boardCheck.includes('xxx')) {
+            setTimeout( () => {
+                alert('X wygrywa');
+                initGame();
+                return;
+            }, 100);
+
+        }
+        if (boardCheck.includes('ooo')) {
+            setTimeout( () => {
+                alert('O wygrywa');
+                initGame();
+                return;
+            }, 100);
+
+        }
+        if (!emptyFields) {
+            setTimeout( () => {
+                alert('Remis');
+                initGame();
+                return;
+            }, 100);
+
+        }
     };
 
     initGame();
